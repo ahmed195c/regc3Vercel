7eea9bd0b691bede20e52f06f6ba3674544a3d36
ahmed195c <102747266+ahmed195c@users.noreply.github.com>
2025-01-12 18:37:17 +0400

    data1 ttest

--- parkingLogsapp/settings.py
+++ parkingLogsapp/settings.py
@@ -74,14 +74,27 @@ WSGI_APPLICATION = 'parkingLogsapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'k9Z1oiMtcOdj',
+        'HOST': 'ep-lucky-wood-a4fcgjae-pooler.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
